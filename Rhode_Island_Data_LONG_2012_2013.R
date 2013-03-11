aab06ef3b40b82b97f616917ab2e4146307c31fa
dbetebenner <dbetebenner@gmail.com>
2013-03-11 04:37:14 -0400

    Update of source code associated with Rhode Island analyses

--- Rhode_Island_Data_LONG_2012_2013.R
+++ Rhode_Island_Data_LONG_2012_2013.R
@@ -28,7 +28,7 @@ load("Data/Rhode_Island_SGP.Rdata")
 
 names(tmp.data) <- toupper(names(tmp.data))
 tmp.variables <- c("RPTSTUDID", "LNAME", "FNAME", "GRADE", "DISCODE", "SCHCODE", "SPRDISCODE", "SPRSCHCODE",
-                   "GENDER", "ETHNIC", "LEP", "IEP", "SES", "REASCALEDSCORE", "REAAL", "MATSCALEDSCORE", "MATAL", "SENDDISCODE", 
+                   "GENDER", "RACE7", "LEP", "IEP", "SES", "REASCALEDSCORE", "REAAL", "MATSCALEDSCORE", "MATAL", "SENDDISCODE", 
 		   "REATESTSTATUS", "MATTESTSTATUS", "SPRCONTSCH", "SPRCONTDIS", "STUSTATUS", "IEPEXITSTATUS")
 tmp.data <- subset(tmp.data, select=intersect(tmp.variables, names(tmp.data)))
 
@@ -45,9 +45,9 @@ read_1213 <- data.frame(ID=RPTSTUDID,
 			DISTRICT_NUMBER_CURRENT=DISCODE,
 			SCHOOL_NUMBER_CURRENT=SCHCODE,
                         GENDER=GENDER,
-                        ETHNICITY=ETHNIC,
+                        ETHNICITY=RACE7,
                         IEP_STATUS=IEP,
-#                        IEP_EXIT_STATUS=IEPEXITSTATUS,
+                        IEP_EXIT_STATUS=IEPEXITSTATUS,
                         ELL_STATUS=LEP,
                         FREE_REDUCED_LUNCH_STATUS=SES,
                         SCALE_SCORE=REASCALEDSCORE,
@@ -69,9 +69,9 @@ math_1213 <- data.frame(ID=RPTSTUDID,
 			DISTRICT_NUMBER_CURRENT=DISCODE,
 			SCHOOL_NUMBER_CURRENT=SCHCODE,
                         GENDER=GENDER,
-                        ETHNICITY=ETHNIC,
+                        ETHNICITY=RACE7,
                         IEP_STATUS=IEP,
-#                        IEP_EXIT_STATUS=IEPEXITSTATUS,
+                        IEP_EXIT_STATUS=IEPEXITSTATUS,
                         ELL_STATUS=LEP,
                         FREE_REDUCED_LUNCH_STATUS=SES,
                         SCALE_SCORE=MATSCALEDSCORE,
@@ -157,6 +157,15 @@ Rhode_Island_Data_LONG_2012_2013$ETHNICITY <- factor(Rhode_Island_Data_LONG_2012
 
 levels(Rhode_Island_Data_LONG_2012_2013$IEP_STATUS) <- c("Students without Disabilities (Non-IEP)", "Students with Disabilities (IEP)")
 
+# IEP EXIT_STATUS
+
+Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS[Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS==" "] <- NA
+Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS <- as.character(Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS)
+Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS[!is.na(Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS)] <- 
+	paste("IEP Exit Status:", Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS[!is.na(Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS)])
+Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS <- factor(Rhode_Island_Data_LONG_2012_2013$IEP_EXIT_STATUS)
+
+
 # LEP STATUS
 
 Rhode_Island_Data_LONG_2012_2013$ELL_STATUS[Rhode_Island_Data_LONG_2012_2013$ELL_STATUS %in% 1:3] <- 1
@@ -223,11 +232,11 @@ Rhode_Island_Data_LONG_2012_2013$VALID_CASE[is.na(Rhode_Island_Data_LONG_2012_20
 Rhode_Island_Data_LONG_2012_2013$VALID_CASE[Rhode_Island_Data_LONG_2012_2013$TEST_STATUS!="A"] <- "INVALID_CASE"
 Rhode_Island_Data_LONG_2012_2013$VALID_CASE[Rhode_Island_Data_LONG_2012_2013$STUDENT_STATUS!=0] <- "INVALID_CASE"
 
+
 # Convert to data.table
 
 Rhode_Island_Data_LONG_2012_2013 <- as.data.table(Rhode_Island_Data_LONG_2012_2013)
 setkeyv(Rhode_Island_Data_LONG_2012_2013, c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"))
-
 
 
 # Tidy up district.codes.2013 and school.codes.2013
@@ -242,6 +251,12 @@ levels(school.codes.2013$SCHOOL_NAME) <- sapply(school.codes.2013$SCHOOL_NAME, c
 school.codes.2013 <- as.data.table(school.codes.2013)
 setkey(school.codes.2013, DISTRICT_NUMBER, SCHOOL_NUMBER)
 Rhode_Island_District_and_School_Names <- school.codes.2013
+
+
+### Make SCHOOL_NUMBER and DISTRICT_NUMBER NA equal to 88 or 88888
+
+Rhode_Island_Data_LONG_2012_2013[['SCHOOL_NUMBER']][is.na(Rhode_Island_Data_LONG_2012_2013[['SCHOOL_NUMBER']]) & Rhode_Island_Data_LONG_2012_2013[['YEAR']]=="2012_2013"] <- "88888"
+Rhode_Island_Data_LONG_2012_2013[['DISTRICT_NUMBER']][is.na(Rhode_Island_Data_LONG_2012_2013[['DISTRICT_NUMBER']]) & Rhode_Island_Data_LONG_2012_2013[['YEAR']]=="2012_2013"] <- "88"
 
 
 #############################################################################
@@ -266,5 +281,5 @@ dups <- Rhode_Island_Data_LONG_2012_2013[Rhode_Island_Data_LONG_2012_2013[J("VAL
 ### Convert to data.frame and save results
 
 Rhode_Island_Data_LONG_2012_2013 <- as.data.frame(Rhode_Island_Data_LONG_2012_2013)
-Rhode_Island_Data_LONG_2012_2013 <- Rhode_Island_Data_LONG_2012_2013[,names(sort(sapply(names(Rhode_Island_Data_LONG_2012_2013), function(x) match(x, names(Rhode_Island_SGP@Data)))))]
+Rhode_Island_Data_LONG_2012_2013 <- Rhode_Island_Data_LONG_2012_2013[,c(names(sort(sapply(names(Rhode_Island_Data_LONG_2012_2013), function(x) match(x, names(Rhode_Island_SGP@Data))))), "IEP_EXIT_STATUS")]
 save(Rhode_Island_Data_LONG_2012_2013, file="Data/Rhode_Island_Data_LONG_2012_2013.Rdata")
