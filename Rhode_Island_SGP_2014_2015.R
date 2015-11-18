5b17e301f5ce6c20c4a9833cf0c4b30099d6c023
dbetebenner <dbetebenner@gmail.com>
2015-11-18 16:15:52 -0500

    Update of configurations

--- Rhode_Island_SGP_2014_2015.R
+++ Rhode_Island_SGP_2014_2015.R
@@ -22,12 +22,12 @@ source("SGP_CONFIG/2014_2015/READING.R")
 source("SGP_CONFIG/2014_2015/MATHEMATICS.R")
 
 
-PARCC_CONFIG <- c(READING_2014_2015.config, MATHEMATICS_2014_2015.config)
+RI_CONFIG <- c(READING_2014_2015.config, MATHEMATICS_2014_2015.config)
 
 
 ### updateSGP
 
-Rhode_Island_PARCC_SGP <- updateSGP(
+Rhode_Island_SGP <- updateSGP(
 			Rhode_Island_SGP,
 			Rhode_Island_Data_LONG_2014_2015,
 			sgp.percentiles=TRUE,
@@ -38,10 +38,10 @@ Rhode_Island_PARCC_SGP <- updateSGP(
 			sgp.projections.lagged.baseline=FALSE,
 			sgp.percentiles.equated=TRUE,
 			sgp.percentiles.equating.method=c("identity", "mean", "linear", "equipercentile"),
-			sgp.config=RI_PARCC_CONFIG,
+			sgp.config=RI_CONFIG,
 			sgp.target.scale.scores=TRUE,
 			sgPlot.demo.report=TRUE,
-			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=8)))
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=2, PROJECTIONS=2, LAGGED_PROJECTIONS=2)))
 
 
 ### Save results
--- SGP_CONFIG/2014_2015/MATHEMATICS.R
+++ SGP_CONFIG/2014_2015/MATHEMATICS.R
@@ -1,15 +1,15 @@
-MATHEMATICS_2015.config <- list(
+MATHEMATICS_2014_2015.config <- list(
     MATHEMATICS.2015 = list(
                 sgp.content.areas=rep('MATHEMATICS', 6),
-                sgp.panel.years=as.character(2010:2015),
+                sgp.panel.years=paste(2009:2014, 2010:2015, sep="_"),
                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))),
 	GEOMETRY.2015 = list(
                 sgp.content.areas=c(rep('MATHEMATICS', 5), 'GEOMETRY'),
-                sgp.panel.years=as.character(2010:2015),
+                sgp.panel.years=paste(2009:2014, 2010:2015, sep="_"),
                 sgp.grade.sequences=list(c('3', '4', '5', '6', '8', 'EOCT')),
-                sgp.projection.grade.sequences="NO_PROJECTIONS"))
+                sgp.projection.grade.sequences="NO_PROJECTIONS"),
 	ALGEBRA_I.2015 = list(
                 sgp.content.areas=c(rep('MATHEMATICS', 5), 'ALGEBRA_I'),
-                sgp.panel.years=as.character(2010:2015),
+                sgp.panel.years=paste(2009:2014, 2010:2015, sep="_"),
                 sgp.grade.sequences=list(c('3', '4', '5', '6', '7', 'EOCT'), c('4', '5', '6', '7', '8', 'EOCT')),
-                sgp.projection.grade.sequences="NO_PROJECTIONS"))
+                sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 2))))
