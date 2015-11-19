d41ee271b63b194457b0d45f3fd2762b51ffe93f
dbetebenner <dbetebenner@gmail.com>
2015-11-18 20:09:05 -0500

    Fixing up parallel.config

--- Rhode_Island_SGP_2014_2015.R
+++ Rhode_Island_SGP_2014_2015.R
@@ -42,7 +42,7 @@ Rhode_Island_SGP <- updateSGP(
 			sgp.target.scale.scores=TRUE,
 			sgPlot.demo.report=TRUE,
 			save.intermediate.results=FALSE,
-			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=2, PROJECTIONS=2, LAGGED_PROJECTIONS=2)))
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=2, PROJECTIONS=2, LAGGED_PROJECTIONS=2, SGP_SCALE_SCORE_TARGETS=2, SG_PLOTS=1)))
 
 
 ### Save results
