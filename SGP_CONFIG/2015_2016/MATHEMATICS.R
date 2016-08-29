1c5f0f48decea6a8ddc15857bab921191932096c
dbetebenner <dbetebenner@gmail.com>
2016-08-29 13:37:13 -0400

    Adding SCHOOLVIEW EXPORT file

--- SGP_CONFIG/2015_2016/MATHEMATICS.R
+++ SGP_CONFIG/2015_2016/MATHEMATICS.R
@@ -1,14 +1,14 @@
 MATHEMATICS_2015_2016.config <- list(
-    MATHEMATICS.2016 = list(
+    MATHEMATICS.2015_2016 = list(
                 sgp.content.areas=rep('MATHEMATICS', 2),
                 sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
                 sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'))),
-	GEOMETRY.2016 = list(
+	GEOMETRY.2015_2016 = list(
                 sgp.content.areas=c('ALGEBRA_I', 'GEOMETRY'),
                 sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
                 sgp.grade.sequences=list(c('EOCT', 'EOCT')),
                 sgp.projection.grade.sequences=as.list("NO_PROJECTIONS")),
-	ALGEBRA_I.2016 = list(
+	ALGEBRA_I.2015_2016 = list(
                 sgp.content.areas=c(rep('MATHEMATICS', 1), 'ALGEBRA_I'),
                 sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
                 sgp.grade.sequences=list(c('7', 'EOCT'), c('8', 'EOCT')),
