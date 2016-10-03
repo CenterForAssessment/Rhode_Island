d563a1292a4c326a2e0a9e93c47daca1dfdf81a3
dbetebenner <dbetebenner@gmail.com>
2016-10-03 15:35:44 -0400

    Updating SCHOOLVIEW export

--- Rhode_Island_SGP_2015_2016_SCHOOLVIEW_EXPORT.R
+++ Rhode_Island_SGP_2015_2016_SCHOOLVIEW_EXPORT.R
@@ -42,7 +42,9 @@ Rhode_Island_SGP <- summarizeSGP(
 
 ### Remove students from schools not suitable for reporting
 
-slot.data[SCHOOL_NUMBER %in% bad_schools$SCHOOL_NUMBER, VALID_CASE:="INVALID_CASE"]
+setkey(bad_schools, YEAR, DISTRICT_NUMBER, SCHOOL_NUMBER)
+setkey(slot.data, YEAR, DISTRICT_NUMBER, SCHOOL_NUMBER)
+slot.data[bad_schools, VALID_CASE:="INVALID_CASE"]
 Rhode_Island_SGP@Data <- slot.data
 setkey(Rhode_Island_SGP@Data, VALID_CASE, CONTENT_AREA, YEAR, ID)
 
