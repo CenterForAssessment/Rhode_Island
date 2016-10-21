60b37cc45cfe21ecd45830953786932e86acfcf0
dbetebenner <dbetebenner@gmail.com>
2016-10-21 14:28:54 -0400

    Closing a parenthesis

--- SGP_CONFIG/2015_2016/ELA.R
+++ SGP_CONFIG/2015_2016/ELA.R
@@ -2,4 +2,4 @@ ELA_2015_2016.config <- list(
         ELA.2015_2016 = list(
                 sgp.content.areas=rep('ELA', 2),
                 sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
-                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10')))
+                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'))))
