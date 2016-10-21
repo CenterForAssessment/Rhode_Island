807f755849ed9f9e98cbdde917907b1027091b77
dbetebenner <dbetebenner@gmail.com>
2016-10-21 13:51:57 -0400

    .

--- SGP_CONFIG/2015_2016/MATHEMATICS.R
+++ SGP_CONFIG/2015_2016/MATHEMATICS.R
@@ -3,13 +3,11 @@ MATHEMATICS_2015_2016.config <- list(
                 sgp.content.areas=rep('MATHEMATICS', 2),
                 sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
                 sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'))),
-	GEOMETRY.2015_2016 = list(
-                sgp.content.areas=c('ALGEBRA_I', 'GEOMETRY'),
-                sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
-                sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-                sgp.projection.grade.sequences=as.list("NO_PROJECTIONS")),
 	ALGEBRA_I.2015_2016 = list(
                 sgp.content.areas=c(rep('MATHEMATICS', 1), 'ALGEBRA_I'),
                 sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
-                sgp.grade.sequences=list(c('7', 'EOCT'), c('8', 'EOCT')),
-                sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 2))))
+                sgp.grade.sequences=list(c('7', 'EOCT'), c('8', 'EOCT'))),
+	GEOMETRY.2015_2016 = list(
+                sgp.content.areas=c('ALGEBRA_I', 'GEOMETRY'),
+                sgp.panel.years=paste(2014:2015, 2015:2016, sep="_"),
+                sgp.grade.sequences=list(c('EOCT', 'EOCT'))))
