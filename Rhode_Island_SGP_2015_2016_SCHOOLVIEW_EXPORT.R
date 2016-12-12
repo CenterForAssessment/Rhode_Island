f87f879afee283d8a9afe99c9fe6ed3a05201225
dbetebenner <dbetebenner@gmail.com>
2016-12-12 12:22:47 -0500

    Another modification to SchoolView output

--- Rhode_Island_SGP_2015_2016_SCHOOLVIEW_EXPORT.R
+++ Rhode_Island_SGP_2015_2016_SCHOOLVIEW_EXPORT.R
@@ -18,22 +18,36 @@ load("Data/Base_Files/bad_schools.Rdata")
 
 ### Change SGPstateData for MATHEMATICS adding ALGEBRA_I & GEOMETRY as MATHEMATICS GRADE 9 and GRADE 10
 
-SGPstateData[['RI']][['Achievement']][['Cutscores']][['MATHEMATICS']][['GRADE_11']] <- SGPstateData[['RI']][['Achievement']][['Cutscores']][['ALGEBRA_I']][['GRADE_EOCT']]
-SGPstateData[['RI']][['Achievement']][['Cutscores']][['MATHEMATICS']][['GRADE_12']] <- SGPstateData[['RI']][['Achievement']][['Cutscores']][['GEOMETRY']][['GRADE_EOCT']]
-SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['knots_11']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['knots_EOCT']]
-SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['boundaries_11']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['boundaries_EOCT']]
-SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['loss.hoss_11']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['loss.hoss_EOCT']]
-SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['knots_12']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['GEOMETRY']][['knots_EOCT']]
-SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['boundaries_12']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['GEOMETRY']][['boundaries_EOCT']]
-SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['loss.hoss_12']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['GEOMETRY']][['loss.hoss_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Cutscores']][['MATHEMATICS']][['GRADE_11']] <- SGPstateData[['RI']][['Achievement']][['Cutscores']][['ALGEBRA_I']][['GRADE_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Cutscores']][['MATHEMATICS']][['GRADE_12']] <- SGPstateData[['RI']][['Achievement']][['Cutscores']][['GEOMETRY']][['GRADE_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['knots_11']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['knots_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['boundaries_11']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['boundaries_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['loss.hoss_11']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['loss.hoss_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['knots_12']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['GEOMETRY']][['knots_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['boundaries_12']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['GEOMETRY']][['boundaries_EOCT']]
+#SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['loss.hoss_12']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['GEOMETRY']][['loss.hoss_EOCT']]
 
 
 ### Recode CONTENT_AREA and GRADE variables for ALGEBRA_I and GEOMETRY
 
+#slot.data <- copy(Rhode_Island_SGP@Data)
+#slot.data[,CONTENT_AREA_ORIGINAL:=CONTENT_AREA]
+#slot.data[CONTENT_AREA=="ALGEBRA_I", c("CONTENT_AREA", "GRADE"):=list("MATHEMATICS", "11")]
+#slot.data[CONTENT_AREA=="GEOMETRY", c("CONTENT_AREA", "GRADE"):=list("MATHEMATICS", "12")]
+#slot.data[!CONTENT_AREA %in% c("ELA", "MATHEMATICS"), VALID_CASE:="INVALID_CASE"]
+#setkey(slot.data, VALID_CASE, CONTENT_AREA, YEAR, ID, GRADE, SGP)
+#setkey(slot.data, VALID_CASE, CONTENT_AREA, YEAR, ID)
+#slot.data[which(duplicated(slot.data, by=key(slot.data)))-1, VALID_CASE:="INVALID_CASE"]
+#setkey(bad_schools, YEAR, DISTRICT_NUMBER, SCHOOL_NUMBER)
+#setkey(slot.data, YEAR, DISTRICT_NUMBER, SCHOOL_NUMBER)
+#slot.data[bad_schools, VALID_CASE:="INVALID_CASE"]
+#setkey(slot.data, VALID_CASE, CONTENT_AREA, YEAR, ID)
+
+### Recode CONTENT_AREA and GRADE variables for ALGEBRA_I
+
 slot.data <- copy(Rhode_Island_SGP@Data)
 slot.data[,CONTENT_AREA_ORIGINAL:=CONTENT_AREA]
-slot.data[CONTENT_AREA=="ALGEBRA_I", c("CONTENT_AREA", "GRADE"):=list("MATHEMATICS", "11")]
-slot.data[CONTENT_AREA=="GEOMETRY", c("CONTENT_AREA", "GRADE"):=list("MATHEMATICS", "12")]
+slot.data[CONTENT_AREA=="ALGEBRA_I" & GRADE_ENROLLED=="8", c("CONTENT_AREA", "GRADE"):=list("MATHEMATICS", "8")]
 slot.data[!CONTENT_AREA %in% c("ELA", "MATHEMATICS"), VALID_CASE:="INVALID_CASE"]
 setkey(slot.data, VALID_CASE, CONTENT_AREA, YEAR, ID, GRADE, SGP)
 setkey(slot.data, VALID_CASE, CONTENT_AREA, YEAR, ID)
@@ -42,7 +56,6 @@ setkey(bad_schools, YEAR, DISTRICT_NUMBER, SCHOOL_NUMBER)
 setkey(slot.data, YEAR, DISTRICT_NUMBER, SCHOOL_NUMBER)
 slot.data[bad_schools, VALID_CASE:="INVALID_CASE"]
 setkey(slot.data, VALID_CASE, CONTENT_AREA, YEAR, ID)
-
 
 ### Put slot.data into @Data
 
@@ -65,13 +78,28 @@ save(Rhode_Island_SGP, file="Data/SchoolView/Rhode_Island_SGP_2015_2016_SCHOOLVI
 
 
 ###
+### Undo ALGEBRA_I as MATHEMATICS GRADE 8 for export
+###
+
+SGPstateData[['RI']][['Achievement']][['Cutscores']][['MATHEMATICS']][['GRADE_9']] <- SGPstateData[['RI']][['Achievement']][['Cutscores']][['ALGEBRA_I']][['GRADE_EOCT']]
+SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['knots_9']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['knots_EOCT']]
+SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['boundaries_9']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['boundaries_EOCT']]
+SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['MATHEMATICS']][['loss.hoss_9']] <- SGPstateData[['RI']][['Achievement']][['Knots_Boundaries']][['ALGEBRA_I']][['loss.hoss_EOCT']]
+
+slot.data <- copy(Rhode_Island_SGP@Data)
+slot.data[CONTENT_AREA_ORIGINAL=="ALGEBRA_I" & GRADE_ENROLLED=="8", c("CONTENT_AREA", "GRADE"):=list("MATHEMATICS", "9")]
+Rhode_Island_SGP@Data <- slot.data
+setkey(Rhode_Island_SGP@Data, VALID_CASE, CONTENT_AREA, YEAR, ID)
+
+
+###
 ### outputSGP
 ###
 
 outputSGP(Rhode_Island_SGP, output.type="SchoolView")
 
 
-### Clean up SCHOOL_NUMBER
+### Clean up OUTPUT
 
 tmp.files <- c("SCHOOL.dat", "SCHOOL_ETHNICITY.dat", "SCHOOL_GRADE.dat", "SCHOOL_STUDENTGROUP.dat")
 for (tmp.iter in tmp.files) {
@@ -83,6 +111,8 @@ for (tmp.iter in tmp.files) {
 load("Data/SchoolView/RDATA/STUDENT_GROWTH.Rdata")
 STUDENT_GROWTH[,SCHOOL_NUMBER:=strtail(SCHOOL_NUMBER, -3)]
 STUDENT_GROWTH <- STUDENT_GROWTH[!is.na(as.numeric(STATE_ASSIGNED_ID))]
+STUDENT_GROWTH <- STUDENT_GROWTH[CONTENT_AREA==2 & GRADE_LEVEL_CY=="9", GRADE_LEVEL_CY:="8"]
+STUDENT_GROWTH <- STUDENT_GROWTH[CONTENT_AREA==2 & GRADE_LEVEL_PY1=="9", GRADE_LEVEL_PY1:="8"]
 save(STUDENT_GROWTH, file="Data/SchoolView/RDATA/STUDENT_GROWTH.Rdata")
 unlink("Data/SchoolView/TEXT/STUDENT_GROWTH.dat.zip")
 write.table(STUDENT_GROWTH, file="Data/SchoolView/TEXT/STUDENT_GROWTH.dat", sep="|", row.names=FALSE, quote=FALSE, na="")
