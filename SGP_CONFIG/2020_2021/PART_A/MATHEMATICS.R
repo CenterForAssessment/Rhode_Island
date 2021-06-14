846565f7d2138cb7070f741d0959061c4637081f
Damian Betebenner <dbetebenner@nciea.org>
2021-06-14 05:31:50 -0400

    Updating 2021 configs

--- SGP_CONFIG/2020_2021/PART_A/MATHEMATICS.R
+++ SGP_CONFIG/2020_2021/PART_A/MATHEMATICS.R
@@ -6,5 +6,5 @@ MATHEMATICS_2020_2021.config <- list(
      MATHEMATICS.2020_2021 = list(
                  sgp.content.areas = c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
                  sgp.panel.years = c('2017_2018', '2018_2019', '2020_2021'),
-                 sgp.grade.sequences = list(c('3', '5'), c('3', '4', '6'), c('4', '5', '7'), c('5', '6', '8'), c('7', '8', '10')))
+                 sgp.grade.sequences = list(c('3', '5'), c('3', '4', '6'), c('4', '5', '7'), c('5', '6', '8')))
 )
