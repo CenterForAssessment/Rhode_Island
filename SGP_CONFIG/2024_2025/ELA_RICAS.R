003dfbdd9572af61f28c282c9ac8bfb61ecc8a32
Damian Betebenner <dbetebenner@nciea.org>
2025-06-11 12:08:10 -0400

    Removing erroneous comment

--- SGP_CONFIG/2024_2025/ELA_RICAS.R
+++ SGP_CONFIG/2024_2025/ELA_RICAS.R
@@ -8,4 +8,4 @@ ELA_RICAS_2024_2025.config <- list(
 	ELA_RICAS.2024_2025 = list(
 		sgp.content.areas=rep('ELA', 3),
 		sgp.panel.years=c('2022_2023', '2023_2024', '2024_2025'),
-		sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8')))) ### Using single prior 8th grade analyses to get median SGP of 50
+		sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8'))))
